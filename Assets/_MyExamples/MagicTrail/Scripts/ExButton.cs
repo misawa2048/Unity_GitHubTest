1d2967e13bda769b5bfa6f428d542d335b649a04
misawa2048 <misawa@elix.jp>
2025-07-10 02:59:00 +0900

    リファクタリング、ログ追加

--- Assets/_MyExamples/MagicTrail/Scripts/ExButton.cs
+++ Assets/_MyExamples/MagicTrail/Scripts/ExButton.cs
@@ -2,12 +2,19 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.Events;
 
-public class ExButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class ExButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
 {
+    [Header("References")]
+    public MagicCircleCtrl magicCircleCtrl; // MagicCircleCtrlへの参照
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        // MagicCircleCtrlが設定されていない場合、自動で検索
+        if (magicCircleCtrl == null)
+        {
+            magicCircleCtrl = FindFirstObjectByType<MagicCircleCtrl>();
+        }
     }
 
     // Update is called once per frame
@@ -20,11 +27,47 @@ public class ExButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     public void OnPointerEnter(PointerEventData eventData)
     {
         //Debug.Log("マウスがボタンに入りました");
+        
+        // MagicCircleCtrlのOnButtonEnter()を呼び出す
+        if (magicCircleCtrl != null)
+        {
+            magicCircleCtrl.OnButtonEnter(this.gameObject);
+        }
     }
 
     // マウスがボタンから出た時の処理
     public void OnPointerExit(PointerEventData eventData)
     {
         //Debug.Log("マウスがボタンから出ました");
+        
+        // MagicCircleCtrlのOnButtonExit()を呼び出す
+        if (magicCircleCtrl != null)
+        {
+            magicCircleCtrl.OnButtonExit(this.gameObject);
+        }
+    }
+    
+    // マウスボタンが押された時の処理
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerDown");
+        
+        // MagicCircleCtrlのOnButtonPressed()を呼び出す
+        if (magicCircleCtrl != null)
+        {
+            magicCircleCtrl.OnButtonPressed(this.gameObject);
+        }
+    }
+    
+    // マウスボタンが離された時の処理
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerUp");
+        
+        // MagicCircleCtrlのOnButtonReleased()を呼び出す
+        if (magicCircleCtrl != null)
+        {
+            magicCircleCtrl.OnButtonReleased(this.gameObject);
+        }
     }
 }
--- Assets/_MyExamples/MagicTrail/Scripts/MagicCircleCtrl.cs
+++ Assets/_MyExamples/MagicTrail/Scripts/MagicCircleCtrl.cs
@@ -2,6 +2,14 @@ using UnityEngine;
 
 public class MagicCircleCtrl : MonoBehaviour
 {
+    [Header("Button State")]
+    public bool isButtonPressed = false; // ボタンを押しているかどうかのフラグ
+    
+    [Header("Trail Object")]
+    public GameObject trailObject; // 軌跡を描くためのGameObject
+    [Range(0f, 10f)]
+    public float trailDepthOffset = 0.5f; // 軌跡オブジェクトの奥行オフセット値
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +20,89 @@ public class MagicCircleCtrl : MonoBehaviour
     void Update()
     {
         
+    }
+    
+    // ボタンが押された時に呼ばれる関数
+    public void OnButtonPressed(GameObject buttonObject = null)
+    {
+        isButtonPressed = true;
+        if (buttonObject != null)
+        {
+            Debug.Log($"ボタンが押されました: {buttonObject.name}");
+            
+            // trailObjectをボタンの位置まで移動
+            if (trailObject != null)
+            {
+                Vector3 buttonPosition = buttonObject.transform.position;
+                Vector3 trailPosition = new Vector3(buttonPosition.x, buttonPosition.y, buttonPosition.z + trailDepthOffset);
+                trailObject.transform.position = trailPosition;
+                Debug.Log($"trailObjectを{buttonObject.name}の位置に移動しました（奥行+{trailDepthOffset}）");
+            }
+        }
+        else
+        {
+            Debug.Log("ボタンが押されました");
+        }
+    }
+    
+    // ボタンが離された時に呼ばれる関数
+    public void OnButtonReleased(GameObject buttonObject = null)
+    {
+        isButtonPressed = false;
+        if (buttonObject != null)
+        {
+            Debug.Log($"ボタンが離されました: {buttonObject.name}");
+        }
+        else
+        {
+            Debug.Log("ボタンが離されました");
+        }
+    }
+    
+    // ボタンにマウスが入った時に呼ばれる関数
+    public void OnButtonEnter(GameObject buttonObject = null)
+    {
+        if (buttonObject != null)
+        {
+            Debug.Log($"ボタンにマウスが入りました: {buttonObject.name}");
+            
+            // ボタンが押されたままマウスが入った場合、trailObjectを移動
+            if (isButtonPressed && trailObject != null)
+            {
+                Vector3 buttonPosition = buttonObject.transform.position;
+                Vector3 trailPosition = new Vector3(buttonPosition.x, buttonPosition.y, buttonPosition.z + trailDepthOffset);
+                trailObject.transform.position = trailPosition;
+                Debug.Log($"ボタンが押されたままマウスが入ったため、trailObjectを{buttonObject.name}の位置に移動しました（奥行+{trailDepthOffset}）");
+            }
+        }
+        else
+        {
+            Debug.Log("ボタンにマウスが入りました");
+        }
+    }
+    
+    // ボタンからマウスが出た時に呼ばれる関数
+    public void OnButtonExit(GameObject buttonObject = null)
+    {
+        if (buttonObject != null)
+        {
+            Debug.Log($"ボタンからマウスが出ました: {buttonObject.name}");
+        }
+        else
+        {
+            Debug.Log("ボタンからマウスが出ました");
+        }
+    }
+    
+    // マウス位置をワールド座標に変換してtrailObjectを移動
+    public void MoveTrailObjectToMousePosition()
+    {
+        if (trailObject != null)
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, 10f));
+            trailObject.transform.position = worldPosition;
+            Debug.Log($"trailObjectをマウス位置に移動しました: {worldPosition}");
+        }
     }
 }
