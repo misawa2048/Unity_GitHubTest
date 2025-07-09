961e68f35c87a722475a24ee410dde8cfe10dc81
misawa2048 <misawa@elix.jp>
2025-07-10 02:10:52 +0900

    画面サイズ調整

--- Assets/_MyExamples/MagicTrail/Scripts/ExButton.cs
+++ Assets/_MyExamples/MagicTrail/Scripts/ExButton.cs
@@ -4,32 +4,27 @@ using UnityEngine.Events;
 
 public class ExButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-        public UnityEvent<PointerEventData> m_ExEnterEvent;
-        public UnityEvent<PointerEventData> m_ExExitEvent;
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     // マウスがボタンに入った時の処理
     public void OnPointerEnter(PointerEventData eventData)
     {
         //Debug.Log("マウスがボタンに入りました");
-        m_ExEnterEvent.Invoke(eventData); // イベントを呼び出す
     }
 
     // マウスがボタンから出た時の処理
     public void OnPointerExit(PointerEventData eventData)
     {
         //Debug.Log("マウスがボタンから出ました");
-        m_ExExitEvent.Invoke(eventData); // イベントを呼び出す       
     }
 }
